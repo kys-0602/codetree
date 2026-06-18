a4878dbe0660e8325d54fa5826c544bb8ce0b7be
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-18 21:18:16 +0900

    [Passed] 점수 입출력 (2ms, 0MB)

---
+++ trail0/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int N; std::cin >> N;
+    std::cout << "Your score is " << N << " point.";
+
+    return 0;
+}
