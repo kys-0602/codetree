4da2712b793ddc147b7a2cdb8a49c71681a12320
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-18 21:24:35 +0900

    [Passed] 입력받은 값과 합 출력 (2ms, 0MB)

---
+++ trail0/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int A, B; std::cin >> A >> B;
+    std::cout << A << ' ' << B << ' ' << (A + B);
+
+    return 0;
+}
