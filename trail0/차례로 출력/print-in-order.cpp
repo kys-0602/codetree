2779eb113d8214d99a3e99e5728c9a8e2376d1e6
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-18 22:29:39 +0900

    [Passed] 차례로 출력 (2ms, 0MB)

---
+++ trail0/차례로 출력/print-in-order.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    for (int i = 5; i <= 17; ++i) {
+        std::cout << i << ' ';
+    }
+
+    return 0;
+}
