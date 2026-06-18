c57539947e7a921075db5dce8bd782a7bf15e5b9
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-18 22:08:31 +0900

    [Passed] 물의 상태 (2ms, 1MB)

---
+++ trail0/물의 상태/state-of-water.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int n; std::cin >> n;
+
+    if (n < 0) std::cout << "ice";
+    else if (n >= 100) std::cout << "vapor";
+    else std::cout << "water";
+
+    return 0;
+}
