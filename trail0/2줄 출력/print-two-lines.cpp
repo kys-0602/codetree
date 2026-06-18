96a2e1e124d36ad363f712f000036e789f76ee73
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-18 21:09:28 +0900

    [Passed] 2줄 출력 (2ms, 0MB)

---
+++ trail0/2줄 출력/print-two-lines.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    std::cout << "Hello\nWorld";
+    return 0;
+}
