9a51f9de1cd069b372707cfc32d925df001e833d
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-18 21:58:02 +0900

    [Passed] 음수 구별하기 (2ms, 0MB)

---
+++ trail0/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int N; std::cin >> N;
+
+    std::cout << N;
+    if (N < 0) std::cout << "\nminus";
+
+    return 0;
+}
