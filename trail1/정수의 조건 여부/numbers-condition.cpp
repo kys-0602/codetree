10d58e3ca131213cce577cc81a6e612bbddc9877
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:51:28 +0900

    [Passed] 정수의 조건 여부 (2ms, 0MB)

---
+++ trail1/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int a; std::cin >> a;
+
+    if (a >= 113) std::cout << 1;
+    else std::cout << 0;
+
+    return 0;
+}
