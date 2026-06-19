a9e878d2aa2d3290193793a71e118f98e5904878
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:02:36 +0900

    [Passed] 따옴표 출력 (1ms, 0MB)

---
+++ trail1/따옴표 출력/print-quote.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    std::cout << "He says \"It's a really simple sentence\".";
+
+    return 0;
+}
