0062c8d041078f4125a0500f7ca843853300d815
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 23:27:18 +0900

    [Passed] 0 나올때까지 출력 (2ms, 0MB)

---
+++ trail1/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int input;
+    while (std::cin >> input) {
+        if (input == 0) break;
+
+        std::cout << input << '\n';
+    }
+    
+    return 0;
+}
