34e8b06e3cf008929b10a92366237962fe7884e2
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:08:32 +0900

    [Passed] 변수 출력하기 2 (2ms, 0MB)

---
+++ trail1/변수 출력하기 2/outputing-variables-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int a = 3;
+    char b = 'C';
+
+    std::cout << b << "!.....!" << a;
+
+    return 0;
+}
