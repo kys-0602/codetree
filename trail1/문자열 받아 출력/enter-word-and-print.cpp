6ddf2c860f7ee32f19465a0a24867e193d5d6ac7
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:31:48 +0900

    [Passed] 문자열 받아 출력 (2ms, 0MB)

---
+++ trail1/문자열 받아 출력/enter-word-and-print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    std::string S; std::cin >> S;
+    std::cout << S;
+
+    return 0;
+}
