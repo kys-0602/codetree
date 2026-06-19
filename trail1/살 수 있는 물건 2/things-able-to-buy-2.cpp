c65fd875d92a0b54db5b41abedf975b0a08efaef
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 15:01:47 +0900

    [Passed] 살 수 있는 물건 2 (2ms, 0MB)

---
+++ trail1/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int N; std::cin >> N;
+    if (N >= 3000) std::cout << "book";
+    else if (N >= 1000) std::cout << "mask";
+    else if (N >= 500) std::cout << "pen";
+    else std::cout << "no";
+
+    return 0;
+}
