7b7ddc1dd432af5b7c6c4b7684f9b01371d58c58
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 15:13:50 +0900

    [Passed] 4가지 관계연산자 (2ms, 0MB)

---
+++ trail1/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int A, B; std::cin >> A >> B;
+
+    std::cout << (A >= B) << '\n';
+    std::cout << (A > B) << '\n';
+    std::cout << (B >= A) << '\n';
+    std::cout << (B > A);
+
+    return 0;
+}
