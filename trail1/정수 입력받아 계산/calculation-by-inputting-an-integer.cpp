2aa6c4196c5ac98a67707a52972ce78ec52888b3
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:24:18 +0900

    [Passed] 정수 입력받아 계산 (2ms, 0MB)

---
+++ trail1/정수 입력받아 계산/calculation-by-inputting-an-integer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int a; std::cin >> a;
+    a <<= 1;
+    a += 3;
+    std::cout << a;
+
+    return 0;
+}
