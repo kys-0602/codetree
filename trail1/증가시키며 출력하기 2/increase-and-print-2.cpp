f94527848826824c8e33bf55eb02aa517ba3c9d9
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 20:05:05 +0900

    [Passed] 증가시키며 출력하기 2 (2ms, 0MB)

---
+++ trail1/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int start = 10, end = 26;
+    while (start <= end) {
+        std::cout << start++ << ' ';
+    }
+
+    return 0;
+}
