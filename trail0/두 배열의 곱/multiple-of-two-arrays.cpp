f5729c8d794f059f5b78d3e880bd553833bca5e5
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 13:21:07 +0900

    [Passed] 두 배열의 곱 (2ms, 0MB)

---
+++ trail0/두 배열의 곱/multiple-of-two-arrays.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int arr1[3][3]{ 0 };
+    for (int y = 0; y < 3; ++y) {
+        for (int x = 0; x < 3; ++x) {
+            std::cin >> arr1[y][x];
+        }
+    }
+
+    int arr2[3][3]{ 0 };
+    for (int y = 0; y < 3; ++y) {
+        for (int x = 0; x < 3; ++x) {
+            std::cin >> arr2[y][x];
+        }
+    }
+
+    for (int y = 0; y < 3; ++y) {
+        for (int x = 0; x < 3; ++x) {
+            std::cout << arr1[y][x] * arr2[y][x] << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    return 0;
+}
