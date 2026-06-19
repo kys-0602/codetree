6473b34ad34cb91cd50d3fa3a03cdda12cd0de10
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:15:13 +0900

    [Passed] 변수 값 교체하기 (2ms, 0MB)

---
+++ trail1/변수 값 교체하기/replacing-variable-values.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int a = 3;
+    a = 6;
+    std::cout << a;
+
+    return 0;
+}
