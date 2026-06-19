d269ef8c778f493846b8bfb882668539beea4a8f
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 15:17:30 +0900

    [Passed] 범위 안의 수 (1ms, 0MB)

---
+++ trail1/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int a; std::cin >> a;
+    
+    std::cout << (((a >= 10) && (a <= 20)) ? "yes" : "no");
+
+    return 0;
+}
