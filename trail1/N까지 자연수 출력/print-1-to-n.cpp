1dd7b4289a4ae6d38733a207acb42148d6a16bad
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 20:05:52 +0900

    [Passed] N까지 자연수 출력 (2ms, 0MB)

---
+++ trail1/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int N; std::cin >> N;
+
+    int start = 1;
+    while (start <= N) {
+        std::cout << start++ << ' ';
+    }
+
+    return 0;
+}
