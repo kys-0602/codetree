731f049965451c1cba474cd0bb5ba84e519d5c72
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 15:41:26 +0900

    [Passed] 일 수 구하기 (2ms, 0MB)

---
+++ trail1/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int n; std::cin >> n;
+
+    int daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    std::cout << daysInMonth[n - 1];
+
+    return 0;
+}
