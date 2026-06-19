2f3b9aa019659d27c200c16b0f239a817f8d0402
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:30:17 +0900

    [Passed] 세 실수의 반올림 (2ms, 0MB)

---
+++ trail1/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    float a, b, c; std::cin >> a >> b >> c;
+
+    std::cout.precision(3);
+    std::cout << std::fixed << a << '\n' << b << '\n' << c;
+
+    return 0;
+}
