22f992f9009d5853acbd48791b377bb791c2356b
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 12:30:41 +0900

    [Passed] 직사각형 별표 출력하기 (2ms, 0MB)

---
+++ trail0/직사각형 별표 출력하기/print-stars-in-rectangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int N, M; std::cin >> N >> M;
+
+    for (int n = 0; n < N; ++n) {
+        for (int m = 0; m < M; ++m) {
+            std::cout << '*' << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    return 0;
+}
