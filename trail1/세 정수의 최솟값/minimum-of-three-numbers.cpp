39021c591ad5b24c0251311899a274ea8fd8f8dd
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 15:24:12 +0900

    [Passed] 세 정수의 최솟값 (2ms, 0MB)

---
+++ trail1/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int a, b, c; std::cin >> a >> b >> c;
+
+    int min = c;
+    if ((a <= b && b <= c) || (a <= c && c <= b)) min = a;
+    else if ((b <= a && a <= c) || (b <= c && c <= a)) min = b;
+    else min = c;
+
+    std::cout << min;
+
+    return 0;
+}
