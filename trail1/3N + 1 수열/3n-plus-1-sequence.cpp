142c98f53db99901b98979c355f79b215384fd9f
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 23:44:01 +0900

    [Passed] 3N + 1 수열 (2ms, 0MB)

---
+++ trail1/3N + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int N; std::cin >> N;
+
+    int cnt = 0;
+    while (N != 1) {
+        if (!(N & 1)) N >>= 1;
+        else {
+            N *= 3;
+            N += 1;
+        }
+        cnt += 1;
+    }
+
+    std::cout << cnt;
+    
+    return 0;
+}
