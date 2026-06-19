a3ac0540314256372f307a4bdccb2eefe07d1246
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:07:30 +0900

    [Passed] 세 정수형 변수 선언 (2ms, 0MB)

---
+++ trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int a = 7;
+    int b = 23;
+    int c = 30;
+
+    std::cout << a << " + " << b << " = " << c;
+
+    return 0;
+}
