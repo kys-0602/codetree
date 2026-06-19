c78c08157bc46469cbc3e3b2e9bf2e244c01d6f7
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:45:05 +0900

    [Passed] 합과 평균 (2ms, 0MB)

---
+++ trail1/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int A, B; std::cin >> A >> B;
+
+    std::cout.precision(1);
+    std::cout << (A + B) << ' ' << std::fixed << (A + B) / 2.0f;
+
+    return 0;
+}
