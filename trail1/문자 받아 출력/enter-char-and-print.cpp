d933f371324e727e35752e03b1bf2539c48187c8
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:31:13 +0900

    [Passed] 문자 받아 출력 (2ms, 0MB)

---
+++ trail1/문자 받아 출력/enter-char-and-print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    char c; std::cin >> c;
+    std::cout << c;
+
+    return 0;
+}
