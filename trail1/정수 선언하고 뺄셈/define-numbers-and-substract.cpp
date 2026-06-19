450d09fdcf2750233414c2c85f0b73c1f8b50078
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:06:06 +0900

    [Passed] 정수 선언하고 뺄셈 (1ms, 0MB)

---
+++ trail1/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int a = 97, b = 13;
+    std::cout << a << " - " << b << " = " << a - b;
+
+    return 0;
+}
