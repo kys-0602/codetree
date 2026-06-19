962d7d6ed2b1015219291fd8e5cf55f524e785e5
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 12:20:40 +0900

    [Passed] 1부터의 합 (2ms, 0MB)

---
+++ trail0/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int N; std::cin >> N;
+    
+    int sum = 0;
+    for (int i = 1; i <= 100; ++i) {
+        sum += i;
+        if (sum >= N) {
+            std::cout << i;
+            break;
+        }
+    }
+
+    return 0;
+}
