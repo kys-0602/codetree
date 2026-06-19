ce2ef3fdc120a19d4c185419b35090eb8196610f
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 22:56:49 +0900

    [Passed] 총합 구하기 (2ms, 0MB)

---
+++ trail1/총합 구하기/find-the-total.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int A, B; std::cin >> A >> B;
+
+    int sum = 0;
+    for (; A <= B; ++A) {
+        if (A % 6 == 0 && A % 8 != 0) {
+            sum += A;
+        }
+    }
+    std::cout << sum;
+    
+    return 0;
+}
