758bdbae772686014f90b46fc4d447eff4eeadac
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:45:37 +0900

    [Passed] 세 수의 합과 평균 구하기 (2ms, 0MB)

---
+++ trail1/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int a, b, c; std::cin >> a >> b >> c;
+    int s = a + b + c;
+    int avg = s / 3;
+    std::cout << s << '\n' << avg;
+
+    return 0;
+}
