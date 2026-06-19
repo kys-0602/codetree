ef12ae8491f79eb19cffcaabbcd132837a94fa42
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 15:13:06 +0900

    [Passed] 비교 연산 (2ms, 0MB)

---
+++ trail1/비교 연산/comparison-operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int A, B; std::cin >> A >> B;
+    std::cout << (A >= B) << '\n';
+    std::cout << (A > B) << '\n';
+    std::cout << (B >= A) << '\n';
+    std::cout << (B > A) << '\n';
+    std::cout << (A == B) << '\n';
+    std::cout << (A != B);
+
+    return 0;
+}
