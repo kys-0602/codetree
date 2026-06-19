cf6a4cec0bcf6626ff599831e90071a02435e1ac
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-19 14:47:50 +0900

    [Passed] 큰 수에서 빼기 (2ms, 0MB)

---
+++ trail1/큰 수에서 빼기/subtract-from-large-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int A, B; std::cin >> A >> B;
+    
+    if (A > B) std::cout << A - B;
+    else std::cout << B - A;
+
+    return 0;
+}
