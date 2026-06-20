da6b5c65453e756debb24706f6286c328d524318
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-20 19:53:44 +0900

    [Passed] 일직선 구구단 (2ms, 0MB)

---
+++ trail1/일직선 구구단/one-line-multiple.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int N; std::cin >> N;
+
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= N; ++j) {
+            std::cout << i << " * " << j << " = " << (i * j) << '\n';
+        }
+    }   
+    
+    return 0;
+}
