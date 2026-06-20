9aab222fdd90e8070a0f3aee6a77d12e8a951ca1
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-20 16:27:35 +0900

    [Passed] 별표 출력하기 13 (2ms, 0MB)

---
+++ trail1/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int N; std::cin >> N;
+
+    for (int i = 0; i < N; ++i) {
+        if (i & 1) {
+            for (int j = 0; j < (i / 2) + 1; ++j) std::cout << '*' << ' ';
+        } else {
+            for (int j = 0; j < (N - (i / 2)); ++j) std::cout << '*' << ' ';
+        }
+        std::cout << '\n';
+    }
+    
+    for (int i = N - 1; i >= 0; --i) {
+        if (i & 1) {
+            for (int j = 0; j < (i / 2) + 1; ++j) std::cout << '*' << ' ';
+        } else {
+            for (int j = 0; j < (N - (i / 2)); ++j) std::cout << '*' << ' ';
+        }
+        std::cout << '\n';
+    }
+    
+    return 0;
+}
