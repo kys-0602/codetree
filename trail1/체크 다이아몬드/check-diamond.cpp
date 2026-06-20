522709b8202f1777c6c124405c7b8ba30bb574e1
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-20 15:05:22 +0900

    [Passed] 체크 다이아몬드 (2ms, 0MB)

---
+++ trail1/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int N; std::cin >> N;
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = N - i - 1; j > 0; --j) std::cout << ' ';
+        for (int j = 0; j < i + 1; ++j) std::cout << '*' << ' ';
+        std::cout << '\n';
+    }
+
+    for (int i = 0; i < N - 1; ++i) {
+        for (int j = 0; j < i + 1; ++j) std::cout << ' ';
+        for (int j = N - i - 1; j > 0; --j) std::cout << '*' << ' ';
+        std::cout << '\n';
+    }
+    
+    return 0;
+}
