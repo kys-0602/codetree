de174569460ded7b283ef95111bfff22d78a4f3a
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-20 14:25:52 +0900

    [Passed] 직각 삼각형 출력 (2ms, 0MB)

---
+++ trail1/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int N; std::cin >> N;
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < (2 * i) + 1; ++j) {
+            std::cout << '*';
+        }
+        std::cout << '\n';
+    }
+    
+    return 0;
+}
