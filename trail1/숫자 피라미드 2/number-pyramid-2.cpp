7522347199c73f500a85c91858e89ff8485e8aad
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-20 19:29:19 +0900

    [Passed] 숫자 피라미드 2 (2ms, 1MB)

---
+++ trail1/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int N; std::cin >> N;
+
+    int start_num = 1;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < i + 1; ++j) {
+            std::cout << start_num++ << ' ';
+        }
+        std::cout << '\n';
+    }
+    
+    return 0;
+}
