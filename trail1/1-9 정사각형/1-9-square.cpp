ad1578ccf9a7402c8f2ebee4c0f6dbac50a960cf
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-20 17:26:41 +0900

    [Passed] 1-9 정사각형 (2ms, 0MB)

---
+++ trail1/1-9 정사각형/1-9-square.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int N; std::cin >> N;
+
+    int cnt = 1;
+    for (int y = 0; y < N; ++y) {
+        for (int x = 0; x < N; ++x) {
+            std::cout << cnt++;
+            if (cnt >= 10) cnt = 1;
+        }
+        std::cout << '\n';
+    }
+    
+    return 0;
+}
