d614b52be92e0ba8bffd1a428e6ca8f4d5d10834
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-20 15:07:19 +0900

    [Passed] 별표 출력하기 8 (2ms, 0MB)

---
+++ trail1/별표 출력하기 8/print-star-8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int N; std::cin >> N;
+
+    for (int i = 0; i < N; ++i) {
+        if (!(i & 1)) std::cout << '*';
+        else for (int j = 0; j < i + 1; ++j) std::cout << '*' << ' ';
+        std::cout << '\n';
+    }
+    
+    return 0;
+}
