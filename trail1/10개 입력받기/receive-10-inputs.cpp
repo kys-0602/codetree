895b0f322ec4a2d8cb14bdd9f4246645633e0993
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-20 21:12:00 +0900

    [Passed] 10개 입력받기 (2ms, 1MB)

---
+++ trail1/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    int sum = 0, last_idx = 10;
+    for (int i = 0, x = 0; i < 10; ++i) {
+        std::cin >> x;
+        if (x == 0) {
+            last_idx = i;
+            break;
+        }
+        sum += x;
+    }
+
+    std::cout.precision(1);
+    std::cout << sum << ' ' << std::fixed << (sum / (float)last_idx);
+    
+    return 0;
+}
