2c589de8e5444bed055be30591693fd13a10fa11
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-21 20:42:21 +0900

    [Passed] 단어로 구분하기 (2ms, 0MB)

---
+++ trail1/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::string strs[10];
+    for (int i = 0; i < 10; ++i) std::cin >> strs[i];
+    for (int i = 0; i < 10; ++i) std::cout << strs[i] << '\n';
+
+    return 0;
+}
