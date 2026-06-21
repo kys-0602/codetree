6ae2db167b949f0eeb7204d9f6d4addfcb46d7ad
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-21 22:06:03 +0900

    [Passed] a로 채워넣기 (2ms, 0MB)

---
+++ trail1/a로 채워넣기/filling-with-a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::string str; std::cin >> str;
+    str[1] = str[str.length() - 2] = 'a';
+    std::cout << str;
+
+    return 0;
+}
