764eb640c3abc779678ed5ba4fb3742d22488dcc
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-21 23:03:40 +0900

    [Passed] 정수만 추출하기 (2ms, 0MB)

---
+++ trail1/정수만 추출하기/extract-only-integers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    std::string str1; std::cin >> str1;
+    std::string str2; std::cin >> str2;
+
+    std::string str1_num = "";
+    for (const auto& ch : str1) {
+        if (ch >= '0' && ch <= '9') str1_num += ch;
+        else break;
+    }
+
+    std::string str2_num = "";
+    for (const auto& ch : str2) {
+        if (ch >= '0' && ch <= '9') str2_num += ch;
+        else break;
+    }
+
+    std::cout << std::stol(str1_num) + std::stol(str2_num);
+
+    return 0;
+}
