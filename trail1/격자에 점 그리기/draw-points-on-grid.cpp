7e0a9788e9ab4ec921c012911c30228bca6c4273
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-21 18:14:12 +0900

    [Passed] 격자에 점 그리기 (2ms, 0MB)

---
+++ trail1/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int arr[9][9]{ 0 };
+    int N, M; std::cin >> N >> M;
+
+    for (int m = 0; m < M; ++m) {
+        int r, c; std::cin >> r >> c;
+
+        arr[r - 1][c - 1] = m + 1;
+    }
+
+    for (int y = 0; y < N; ++y) {
+        for (int x = 0; x < N; ++x) {
+            std::cout << arr[y][x] << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    return 0;
+}
