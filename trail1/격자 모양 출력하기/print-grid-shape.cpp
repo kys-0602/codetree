a17688fa50fd863f911f16bddad34fb0f27f2aae
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-21 18:16:20 +0900

    [Passed] 격자 모양 출력하기 (2ms, 0MB)

---
+++ trail1/격자 모양 출력하기/print-grid-shape.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int arr[10][10]{ 0 };
+    int N, M; std::cin >> N >> M;
+
+    for (int m = 0; m < M; ++m) {
+        int r, c; std::cin >> r >> c;
+
+        arr[r - 1][c - 1] = r * c;
+    }
+    
+    for (int y = 0; y < N; ++y) {
+        for (int x = 0; x < N; ++x) {
+            std::cout << arr[y][x] << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    return 0;
+}
