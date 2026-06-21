11b4c3cac1ceaae2d1b65aaf99290c5fbac9f34b
__kys__ <36888398+kys-0602@users.noreply.github.com>
2026-06-21 23:52:35 +0900

    [Passed] 최소공배수 구하기 (2ms, 0MB)

---
+++ trail2/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int t = b;
+        b = a % b;
+        a = t;
+    }
+
+    return a;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int n, m; std::cin >> n >> m;
+    std::cout << (n * m) / gcd(n, m);
+
+    return 0;
+}
